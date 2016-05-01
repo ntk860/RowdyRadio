1ebef83aca4f271936827ed721329deb894abbe3
EvanilsonBraulio <evanilsonbraulio@gmail.com>
2016-05-01 13:52:24 -0500

    my branch created

--- app/src/main/java/rowdy/utsa/group3/rowdyradio/mainactivity.java
+++ app/src/main/java/rowdy/utsa/group3/rowdyradio/mainactivity.java
@@ -1,13 +1,12 @@
 package rowdy.utsa.group3.rowdyradio;
 
+import android.content.Intent;
+
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class mainactivity extends AppCompatActivity {
 
@@ -21,7 +20,7 @@ public class mainactivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_mainactivity, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -30,5 +29,10 @@ public class mainactivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         return false;
+    }
+
+    public void Login(View view) {
+        //Intent intent = new Intent(mainactivity.this, mainradio.class);
+        //startActivity(intent);
     }
 }
